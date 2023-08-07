0d7819b57040f514432bf8484caecd5e285a90f1
MamakHacker <141401596+MamakHacker@users.noreply.github.com>
2023-08-07 13:48:07 +0800

    Update supabase.dart

--- lib/backend/supabase/supabase.dart
+++ lib/backend/supabase/supabase.dart
@@ -3,9 +3,9 @@ import 'package:supabase_flutter/supabase_flutter.dart' hide Provider;
 export 'database/database.dart';
 export 'storage/storage.dart';
 
-const _kSupabaseUrl = 'https://suxpdstpmohugzgkywaq.supabase.co';
+const _kSupabaseUrl = 'Your Supabase Url';
 const _kSupabaseAnonKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1eHBkc3RwbW9odWd6Z2t5d2FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg4MDY2NzgsImV4cCI6MjAwNDM4MjY3OH0.mw6Ue6vrFHDQdEjXg5QiiFrhqZKpKryjJ-5JZ6BuVS0';
+    'Your Supabase Anon Key';
 
 class SupaFlow {
   SupaFlow._();
